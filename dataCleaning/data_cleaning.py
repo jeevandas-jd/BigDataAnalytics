9500e8c6d721903b9d55c90bdd87cbaf18ea99e2
jeevandas-jd <jeevandasms2004@gmail.com>
2025-07-11 11:32:18 +0530

    datataCleaning

---
+++ dataCleaning/data_cleaning.py
@@ -0,0 +1,12 @@
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import mean
+
+
+spark =SparkSession.builder.appName("titanicDataClean").getOrCreate()
+
+df=spark.read.csv("/home/jd/BigDataAnalytics/datasets/titanic.csv",header=True,inferSchema=True)
+df = df.na.fill({"Age": df.select(mean("Age")).first()[0]})
+df = df.dropDuplicates()
+df.show(5)
